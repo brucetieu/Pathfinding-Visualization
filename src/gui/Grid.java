cebc0dd738ae01325b0dcefd6e651ab8fdc86e42
brucetieu <brucetieu5@gmail.com>
2021-02-20 14:46:26 -0700

    Implement MouseMotionListener

--- src/gui/Grid.java
+++ src/gui/Grid.java
@@ -1,22 +1,45 @@
 package gui;
 
+import pathfinding.Node;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Grid extends JPanel {
 
+public class Grid extends JPanel implements MouseMotionListener {
+    
+
+    public Grid() {
+
+    }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
 
+        // Get current width and size of component.
         int width = getWidth();
         int height = getHeight();
 
+        // Creating 30 x 30 rects which span the size of the frame.
         for ( int x = 0; x <= width; x += 30 ) {
             for ( int y = 0; y <= height; y += 30 ) {
-                g.drawRect(x, y, 30, 30);
+                g2d.drawRect(x, y, 30, 30);
             }
         }
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
     }
 }
