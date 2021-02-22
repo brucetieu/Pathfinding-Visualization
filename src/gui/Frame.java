1cfa90e6096e46209e5658e4f09f0a9d45b6c08e
brucetieu <brucetieu5@gmail.com>
2021-02-21 17:15:49 -0700

    Add controller options

--- src/gui/Frame.java
+++ src/gui/Frame.java
@@ -6,6 +6,8 @@ import java.awt.*;
 public class Frame{
     private JPanel panel;
     private JPanel controllerPanel;
+    private String[] mazeGenerationOptions = {"Random", "Prim's", "Kruskal's"};
+    private JComboBox mazeGenerationOptionList;
 
     public Frame() {
         JFrame frame = new JFrame();
@@ -15,19 +17,23 @@ public class Frame{
         panel.setLayout(new GridLayout());
         panel.setBackground(Color.white);
         controllerPanel.setLayout(new FlowLayout());
-        controllerPanel.setPreferredSize(new Dimension(200, frame.getHeight()));
+        controllerPanel.setPreferredSize(new Dimension(250, frame.getHeight()));
         controllerPanel.setBackground(Color.lightGray);
 
         JButton visualize = new JButton("Visualize");
+        JLabel mazeLabel = new JLabel("Choose a maze creation method");
+        mazeGenerationOptionList = new JComboBox(mazeGenerationOptions);
         JButton generateMaze = new JButton("Generate Maze");
 
 
         controllerPanel.add(visualize);
+        controllerPanel.add(mazeLabel);
+        controllerPanel.add(mazeGenerationOptionList);
         controllerPanel.add(generateMaze);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel, BorderLayout.CENTER);
         frame.add(controllerPanel, BorderLayout.WEST);
-        frame.setSize(1000, 800);
+        frame.setSize(1200, 800);
         frame.setResizable(false);
         frame.setTitle("Pathfinding Visualizer");
         frame.setLocationRelativeTo(null);
