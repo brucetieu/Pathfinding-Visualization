f400a8f3353452890e9a477cc61bbf454ecacef6
brucetieu <brucetieu5@gmail.com>
2021-02-21 17:08:35 -0700

    Update Frame

--- src/gui/Frame.java
+++ src/gui/Frame.java
@@ -3,23 +3,36 @@ package gui;
 import javax.swing.*;
 import java.awt.*;
 
-public class Frame extends JFrame{
-
-    Grid grid = new Grid();
+public class Frame{
+    private JPanel panel;
+    private JPanel controllerPanel;
 
     public Frame() {
+        JFrame frame = new JFrame();
+        frame.setLayout(new BorderLayout());
+        panel = new JPanel();
+        controllerPanel = new JPanel();
+        panel.setLayout(new GridLayout());
+        panel.setBackground(Color.white);
+        controllerPanel.setLayout(new FlowLayout());
+        controllerPanel.setPreferredSize(new Dimension(200, frame.getHeight()));
+        controllerPanel.setBackground(Color.lightGray);
 
-        this.setTitle("Pathfinding Visualizer");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(650, 650);
-        this.add(grid);
-        this.setVisible(true);
+        JButton visualize = new JButton("Visualize");
+        JButton generateMaze = new JButton("Generate Maze");
+
+
+        controllerPanel.add(visualize);
+        controllerPanel.add(generateMaze);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel, BorderLayout.CENTER);
+        frame.add(controllerPanel, BorderLayout.WEST);
+        frame.setSize(1000, 800);
+        frame.setResizable(false);
+        frame.setTitle("Pathfinding Visualizer");
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
 
     }
-
-    public static void main (String[] args) {
-        new Frame();
-    }
-
 
 }
