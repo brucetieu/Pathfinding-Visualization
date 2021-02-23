31b698e925a0e153e3d7d593dfc9b2741081a34e
brucetieu <brucetieu5@gmail.com>
2021-02-22 19:09:45 -0700

    Init add of DFSRandomized

--- src/maze/DFSRandomized.java
+++ src/maze/DFSRandomized.java
@@ -1,2 +1,27 @@
-package maze;public class DFSRandomized {
+package maze;
+
+import pathfinding.Node;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.Random;
+import java.util.Stack;
+
+public class DFSRandomized {
+
+    private Node[][] grid;
+    private JPanel panel;
+
+    public DFSRandomized(Node[][] grid, JPanel panel) {
+        this.grid = grid;
+        this.panel = panel;
+
+        Random random = new Random();
+
+        int r = random.nextInt();
+    }
+
+
+
+
 }
