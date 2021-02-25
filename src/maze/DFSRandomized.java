2f6b1b23cf5d170bfa3db8473aa890c1f1c5d9b1
brucetieu <brucetieu5@gmail.com>
2021-02-24 19:13:30 -0700

    Start recursive dfs maze gen

--- src/maze/DFSRandomized.java
+++ src/maze/DFSRandomized.java
@@ -3,10 +3,13 @@ package maze;
 import pathfinding.Node;
 
 import javax.swing.JPanel;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DFSRandomized extends JPanel {
 
     private Node[][] maze;
+    private boolean[][] visited;
 
     private int height;
     private int width;
@@ -15,6 +18,28 @@ public class DFSRandomized extends JPanel {
         this.maze = maze;
         width = this.maze[0].length;
         height = this.maze.length;
+
+        this.visited = new boolean[][][width][height];
+    }
+
+    public void generateMaze() {
+        this.maze[0][0].setStart(true);
+        this.maze[0][0].setWall(false);
+
+        dfs(0, 0);
+    }
+
+    private void dfs(int row, int col) {
+
+        this.visited[row][col] = true;
+
+
+    }
+
+    private List<Node> findAdjacent(int row, int col) {
+        List<Node> adjNeighbors = new ArrayList<>();
+
+
 
     }
 }
