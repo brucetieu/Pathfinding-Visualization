9c07af6d1c68c0bb8d5ccd384a7f7e31cb1972b8
brucetieu <brucetieu5@gmail.com>
2021-02-27 08:27:52 -0700

    Add MazeUtils to generate start and end nodes

--- src/utils/MazeUtils.java
+++ src/utils/MazeUtils.java
@@ -1,2 +1,85 @@
-package utils;public class MazeUtils {
+package utils;
+
+import maze.Maze;
+import pathfinding.Node;
+
+import java.util.Random;
+
+public class MazeUtils {
+
+//    private Maze maze;
+    private static int r1, r2, c1, c2;
+    private static Random rand = new Random();
+
+//    public MazeUtils(Maze maze) {
+//        this.maze = maze;
+//    }
+
+    public static int getR1() {
+        return r1;
+    }
+
+    public void setR1(int r1) {
+        this.r1 = r1;
+    }
+
+    public static int getR2() {
+        return r2;
+    }
+
+    public void setR2(int r2) {
+        this.r2 = r2;
+    }
+
+    public static int getC1() {
+        return c1;
+    }
+
+    public void setC1(int c1) {
+        this.c1 = c1;
+    }
+
+    public static int getC2() {
+        return c2;
+    }
+
+    public void setC2(int c2) {
+        this.c2 = c2;
+    }
+
+    public static Node generateStartNode() {
+        r1 = rand.nextInt(Maze.MAX_HEIGHT);
+        while (r1 % 2 == 0) r1 = rand.nextInt(Maze.MAX_HEIGHT);
+
+        // Generate random odd col for end node.
+        c1 = rand.nextInt(Maze.MAX_WIDTH);
+        while (c1 % 2 == 0) c1 = rand.nextInt(Maze.MAX_WIDTH);
+
+        return new Node(r1, c1);
+//        return new Node(r1, c1, false, true, false, false, false);
+//        Maze.maze[r1][c1].setStart(true);
+//        Maze.maze[r1][c1].setWall(false);
+
+//        return Maze.maze[r1][c1];
+    }
+
+    /**
+     * Generate an end node at an even place on the maze. Do this so that startNode != endNode.
+     */
+    public static Node generateEndNode() {
+        // Generate random even row for start node.
+        r2 = rand.nextInt(Maze.MAX_HEIGHT);
+        while (r2 % 2 != 0) r2 = rand.nextInt(Maze.MAX_HEIGHT);
+
+        // Generate random even col for end node.
+        c2 = rand.nextInt(Maze.MAX_WIDTH);
+        while (c2 % 2 != 0) c2 = rand.nextInt(Maze.MAX_WIDTH);
+
+//        return new Node(r2, c2, false, false, true, false, false);
+//        Maze.maze[r2][c2].setEnd(true);
+        return new Node(r2, c2);
+//        Maze.maze[r2][c2].setWall(false);
+//
+//        return Maze.maze[r2][c2];
+    }
 }
