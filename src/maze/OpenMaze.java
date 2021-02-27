883c45ca4eb326435417bcc0b6cbbe19b3a7c4ab
brucetieu <brucetieu5@gmail.com>
2021-02-27 09:07:31 -0700

    Fix Open Maze

--- src/maze/OpenMaze.java
+++ src/maze/OpenMaze.java
@@ -1,2 +1,26 @@
-package maze;public class OpenMaze {
+package maze;
+
+import utils.Delay;
+
+/**
+ * Build an open maze with no walls.
+ */
+public class OpenMaze {
+
+    private Maze maze;
+
+    public OpenMaze(Maze maze) {
+        this.maze = maze;
+    }
+
+    public void generateMaze() {
+        for (int row = 0; row < this.maze.getMaze().length; row++) {
+            for (int col = 0; col < this.maze.getMaze()[0].length; col++) {
+                Delay.delay(5);
+                this.maze.update();
+                this.maze.getMaze()[row][col].setWall(false);
+            }
+        }
+    }
+
 }
