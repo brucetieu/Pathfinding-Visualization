1681a4a370cfad50326df1661c1eebb55336e638
brucetieu <brucetieu5@gmail.com>
2021-02-26 17:21:12 -0700

    Add delay to animation

--- src/utils/Delay.java
+++ src/utils/Delay.java
@@ -1,2 +1,14 @@
-package utils;public class Delay {
+package utils;
+
+public class Delay {
+
+    public static void delay(int ms) {
+        try
+        { Thread.sleep(ms); }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
+    }
+
 }
